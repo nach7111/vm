420333506a670d0a67bd5b67e40534c939c7f474
nach7111 <cnathan121269@gmail.com>
2026-06-10 12:29:09 -0500

    Create app.js

---
+++ app.js
@@ -0,0 +1,9 @@
+ const firebaseConfig = {
+    apiKey: "AIzaSyD27_oVOMY_7M7aSOh_H6VP6pUtqaRok4w",
+    authDomain: "wifi-splash-page-9d07b.firebaseapp.com",
+    projectId: "wifi-splash-page-9d07b",
+    storageBucket: "wifi-splash-page-9d07b.firebasestorage.app",
+    messagingSenderId: "915776610963",
+    appId: "1:915776610963:web:7d57b5f0e05c19ce16e366",
+    measurementId: "G-E4WK8167DH"
+  };
